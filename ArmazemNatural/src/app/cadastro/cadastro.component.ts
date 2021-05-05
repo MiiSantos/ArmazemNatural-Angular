854ecd3c730f0fe3080bd087dd2a54e80b45235b
Miria Santos <miriaandressa1@gmail.com>
2021-05-05 15:09:41 -0300

    Consumo de login e cadastro implementados

--- ArmazemNatural/src/app/cadastro/cadastro.component.ts
+++ ArmazemNatural/src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { usuario } from '../model/usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  usuario: usuario = new usuario
+  confirmeSenha: string
+  tipoUsuario: string
 
-  ngOnInit(): void {
+  constructor(
+    private authservice: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+
+  confirmarSenha(event: any){
+    this.confirmeSenha = event.target.value
+  }
+
+  tipoUser(event: any){
+    this.tipoUsuario = event.target.value
+  }
+
+  cadastrar(){
+    this.usuario.tipo = this.tipoUsuario
+
+    if(this.usuario.senha != this.confirmeSenha){
+      alert('As senhas estão incorretas!')
+    } else {
+      this.authservice.cadastrar(this.usuario).subscribe((resp: usuario) => {
+        this.usuario = resp
+        this.router.navigate(['/login'])
+        alert('Usuário cadastrado com sucesso!')
+      })
+    }
   }
 
 }
--- ArmazemNatural/src/app/login/login.component.ts
+++ ArmazemNatural/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { userLogin } from '../model/userLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userLogin: userLogin = new userLogin
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+
+  entrar(){
+    this.auth.entrar(this.userLogin).subscribe((resp: userLogin) => {
+      this.userLogin = resp
+
+      environment.token = this.userLogin.token
+      environment.nome = this.userLogin.email
+      environment.foto = this.userLogin.foto
+
+      this.router.navigate(['/inicio'])
+
+    }, erro => {
+      if(erro.status == 500){
+        alert('Usuário ou senha estão incorretos!')
+      }
+    }
+    )
   }
 
 }
